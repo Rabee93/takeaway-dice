bba61a5abe5d6ee6a45889a41d727cdc8ab758c7
Rabee Mghrabi <learningprogramming2019@gmail.com>
2020-09-02 13:53:37 +0100

    created viewcontroller for the randomizer method

---
+++ takeaway-dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  takeaway-dice
+//
+//  Created by rabei mghrabi on 02/09/2020.
+//  Copyright © 2020 rabee mghrabi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
